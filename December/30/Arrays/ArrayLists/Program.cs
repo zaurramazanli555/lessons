6b2ef3275be7e0408e8caa8c2eff96aab20b5a9c
Orkhan Farajov <orkhankhf@gmail.com>
2021-12-31 00:29:59 +0300

    30 Dec Arrays

---
+++ December/30/Arrays/ArrayLists/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayLists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Adding value to ArrayList and capacity
+            object[] arr = new object[10];
+            arr[0] = "Name";
+            arr[1] = 5;
+
+            ArrayList arrayList = new ArrayList();
+
+            int capacity = 0;
+            int count = 0;
+
+            ArrayList students = new ArrayList();
+            students.Add("Samir");
+            students.Add("Javid");
+            students.Add("Yashar");
+            students.Add("Teymur");
+            students.Add("Gunel");
+            students.Add("Arif");
+            students.Add("Gunel");
+            students.Add("Arif");
+
+            capacity = students.Capacity;
+            count = students.Count;
+
+            students.Add(5);
+            students.Add(true);
+            #endregion
+
+            #region ArrayList AddRange
+            //A1 sinifi
+            ArrayList a1 = new ArrayList();
+            a1.Add("Samir");
+            a1.Add("Javid");
+            a1.Add("Yashar");
+            a1.Add("Teymur");
+            a1.Add("Gunel");
+
+            //A2 sinifi
+            ArrayList a2 = new ArrayList();
+            a2.Add("Dadash");
+            a2.Add("Emil");
+            a2.Add("Ferec");
+
+            //Merge classes
+            a1.AddRange(a2);
+            #endregion
+
+            #region Getting values
+            var javid = a1[1];
+            object teymur = a1[3];
+            string samir = a1[0].ToString();
+            #endregion
+
+            #region Sorting
+            //a1.Reverse();
+            //a1.Sort();
+            #endregion
+
+            #region Delete data
+            //a1.Remove("Samir");
+            //a1.RemoveAt(2);
+            //a1.RemoveRange(2, 2);
+            #endregion
+
+            #region Check data
+            a1.Add("Malik");
+
+            bool ct = a1.Contains("Malik");
+
+            if (a1.Contains("Malik"))
+            {
+                int index = a1.IndexOf("Malik");
+                a1.RemoveAt(index);
+            }
+            #endregion
+
+            #region Clearing Array
+            a1.Clear();
+            //a1.Add("Test");
+            a1.TrimToSize();
+            #endregion
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ December/30/Arrays/Arrays/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * 5 elementli array
+             * 
+             * [0]
+             * [1]
+             * [2]
+             * [3]
+             * [4]
+             */
+
+            #region String Array
+            {
+                string[] studentNameList = new string[5];
+
+                studentNameList[0] = "Samir Samirov";
+                studentNameList[1] = "Ceyhun Ceyhunov";
+                studentNameList[2] = "Kamal Kamalov";
+                studentNameList[3] = "John John";
+                studentNameList[4] = "Eric Eric";
+
+                //Console.WriteLine(studentNameList[1]);
+
+                foreach (var item in studentNameList)
+                {
+                    Console.WriteLine(item);
+                }
+
+                for (int i = 0; i < studentNameList.Length; i++)
+                {
+                    Console.WriteLine(studentNameList[i]);
+                }
+            }
+            #endregion
+
+            #region Integer and Boolean Array
+            {
+                int[] numbers = new int[10];
+
+                bool[] boolArr = new bool[] {
+                    true,
+                    false,
+                    false,
+                    true,
+                    true
+                };
+
+                numbers[0] = 100;
+                numbers[1] = 200;
+                numbers[2] = 300;
+                numbers[3] = 400;
+                numbers[4] = 500;
+                numbers[5] = 600;
+                numbers[6] = 700;
+                numbers[7] = 800;
+                numbers[8] = 900;
+                numbers[9] = 1000;
+
+                Console.WriteLine(numbers[1]);
+
+                foreach (var item in numbers)
+                {
+                    Console.WriteLine(item);
+                }
+
+                for (int i = 0; i < numbers.Length; i++)
+                {
+                    int num = numbers[i];
+                    Console.WriteLine(num);
+                }
+            }
+            #endregion
+
+            #region HomeWork
+            {
+                Console.WriteLine("Xahis olunur massivin elementlerinin sayini daxil edin: ");
+                int arrayLength = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Massivin minimum deyerini daxil edin: ");
+                int startNumber = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Massivin maksimum daxil edin: ");
+                int endNumber = int.Parse(Console.ReadLine());
+
+                int[] numbers = new int[arrayLength];
+                bool[] bools = new bool[arrayLength];
+
+                Random rand = new Random();
+
+                for (int i = 0; i < arrayLength; i++)
+                {
+                    numbers[i] = rand.Next(startNumber, endNumber);
+                    bools[i] = Convert.ToBoolean(rand.Next(0, 2));
+
+                    if (numbers[i] % 2 == 0 && bools[i])
+                        Console.WriteLine(numbers[i] + " - " + bools[i]);
+                }
+                
+                Console.ReadLine();
+            }
+            #endregion
+
+            Console.ReadLine();
+        }
+    }
+}
