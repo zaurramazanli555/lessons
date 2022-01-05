87d8131e417b18ffa310619802d0274a7f4433ef
dfjvn <ccc@cc.cc>
2022-01-05 17:42:53 +0400

    Homework_1_30122021

---
+++ 2021/December/30/Homework1NumberFind/Homework1NumberFind/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework1NumberFind
+{
+    class Program
+    {
+        //Daxil olunmus reqemi nece cehdde tapdigini yazan proqram
+        static void Main(string[] args)
+        {
+            Random q = new Random();
+            int rand = q.Next(1, 11);
+            bool check = false;
+            int count = 0;
+            do
+            {
+                count++;
+                Console.WriteLine(count+" cehd");
+                Console.WriteLine("Zehmet olmasa mueyyen olunmus ededi tapmaq ucun eded daxil edin");
+                int eded = int.Parse(Console.ReadLine());
+                if (eded==rand) 
+                {
+                    check = true;
+                    Console.WriteLine("Dogrudur");
+                }
+                else Console.WriteLine("Yanlisdir");
+
+            } while (check==false);
+            Console.ReadLine();
+        }
+    }
+}
