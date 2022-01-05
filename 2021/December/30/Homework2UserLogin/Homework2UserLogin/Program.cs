f9b589e8737e610048e4000f250580e2335f89d7
dfjvn <ccc@cc.cc>
2022-01-05 17:49:03 +0400

    Homework2_Login

---
+++ 2021/December/30/Homework2UserLogin/Homework2UserLogin/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework2UserLogin
+{
+    class Program
+    {
+        //username and password check demo/demo 
+        static void Main(string[] args)
+        {
+            bool check = false;
+            do
+            {
+                Console.WriteLine("Zehmet olmasa username daxil edin");
+                string username = Console.ReadLine().ToLower();
+                Console.WriteLine("Zehmet olmasa parolunuzu daxil edin");
+                string password = Console.ReadLine().ToLower();
+                if (username=="demo" && password=="demo")
+                {
+                    Console.WriteLine("User login succesfully");
+                    check = true;
+                }
+                else Console.WriteLine("Daxil elediyiniz username veya parol sehcvdir,zehmet olmasa yenidaen cehd edin");
+            } while (check==false);
+            Console.ReadLine();
+        }
+    }
+}
