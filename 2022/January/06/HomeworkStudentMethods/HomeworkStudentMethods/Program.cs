9e305f950f1a28e31aa5bdaa54e3744dfb43363a
dfjvn <ccc@cc.cc>
2022-01-06 16:58:40 +0400

    Homework commit

---
+++ 2022/January/06/HomeworkStudentMethods/HomeworkStudentMethods/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeworkStudentMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student();
+            Console.WriteLine("Zehmet olmasa adinizi daxil ederdiniz!");
+            student.Name = Console.ReadLine();
+            Console.WriteLine("Zehmet olmasa soyadinizi daxil ederdiniz!");
+            student.Surname = Console.ReadLine();
+            Console.WriteLine("Zehmet olmasa 1ci imtahanda yigdiginiz imtahan balinizi daxil ederdiniz!");
+            student.Exam1 =decimal.Parse(Console.ReadLine());
+            Console.WriteLine("Zehmet olmasa 2ci imtahanda yigdiginiz imtahan balinizi daxil ederdiniz!");
+            student.Exam2 = decimal.Parse(Console.ReadLine());
+            Console.WriteLine("Zehmet olmasa 3ci imtahanda yigdiginiz imtahan balinizi daxil ederdiniz!");
+            student.Exam3 = decimal.Parse(Console.ReadLine());
+           // Console.WriteLine(student.CalcAverage(student));
+            Console.WriteLine(student.CalcAverage());
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 2022/January/06/HomeworkStudentMethods/HomeworkStudentMethods/Student.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeworkStudentMethods
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public decimal Exam1 { get; set; }
+        public decimal Exam2 { get; set; }
+        public decimal Exam3 { get; set; }
+        public  decimal SumExam()
+        {
+            return Exam1 + Exam2 + Exam3;
+        }
+        public  bool RateAverage()
+        {
+            decimal sumexam = SumExam()/3;
+            if (sumexam > 45) return true;
+            else return false;
+        }
+        public string CalcAverage(Student student)
+        {
+
+            if(student.RateAverage()) return "Tebrikler " +student.Name+" "+student.Surname+".Siz imtahandan ugurla kecdiniz";
+            return "Teesufler olsun " + student.Name + " " + student.Surname + ".Siz imtahandan kesildiniz";
+        }
+        public string CalcAverage()
+        {
+
+            if (RateAverage()) return "Tebrikler " + this.Name + " " + this.Surname + ".Siz ortalama " +Math.Round((SumExam()/3),2)+ " bal ile imtahandan ugurla kecdiniz";
+            return "Tessuler olsun " + this.Name + " " + this.Surname + ".Siz ortalama " + Math.Round((SumExam() / 3),2) + " bal ile imtahandan kesildiniz";
+        }
+    }
+}
