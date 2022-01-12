e9137437db387e2869a8329e7f39cba7cd5865e7
Orkhan Farajov <orkhankhf@gmail.com>
2022-01-13 00:29:36 +0300

    Encapsulation classroom work

---
+++ 2022/January/11/Encapsulation/Encapsulation/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region What is Encapsulation
+            Customer customer = new Customer();
+            customer.Id = 1;
+            customer.Name = "Orkhan";
+            customer.Surname = "Farajov";
+            customer.Email = "orkhankhf@gmail.com";
+
+            var email = customer.Email;
+            #endregion
+
+            #region Using Encapsulation
+            Student student = new Student();
+            student.Name = "Orkhan";
+            student.Surname = "Farajov";
+            var email1 = student.Email;
+
+            Student student2 = new Student();
+            student2.Name = "Ruslan";
+            student2.Surname = "Valiyev";
+            var email2 = student2.Email;
+            #endregion
+        }
+    }
+}
---
+++ 2022/January/11/Encapsulation/Encapsulation/Student.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation
+{
+    internal class Student
+    {
+        public string _name;
+        private string _surname;
+        private string _email;
+
+        public string Email
+        {
+            get
+            {
+                return this._email;
+            }
+            private set { }
+        }
+
+        public string Surname
+        {
+            get
+            {
+                return this._surname;
+            }
+            set
+            {
+                this._surname = value;
+                if (!string.IsNullOrEmpty(_name))
+                    this._email = string.Format("{0}.{1}@company.com", _name.ToLower(), _surname.ToLower());
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return this._name;
+            }
+            set
+            {
+                this._name = value;
+                if (!string.IsNullOrEmpty(_surname))
+                    this._email = string.Format("{0}.{1}@company.com", _name.ToLower(), _surname.ToLower());
+            }
+        }
+    }
+}
