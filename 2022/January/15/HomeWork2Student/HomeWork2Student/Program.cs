b63470f6db54cb12d244d74940aeb17aa67d2084
Zaur Ramazanli <zaurramazanli555@gmail.com>
2022-01-15 18:06:26 +0300

    Homwork2Add

---
+++ 2022/January/15/HomeWork2Student/HomeWork2Student/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HomeWork2Student
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student st = new Student();
+            Console.WriteLine(st.Id);
+            Console.ReadLine();
+ 
+        }
+    }
+}
---
+++ 2022/January/15/HomeWork2Student/HomeWork2Student/Student.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork2Student
+{
+    class Student
+    {
+        /*
+        
+        Elave olaraq private bir GenerateId methodunuz olsun 
+        ve burada random bir deyeri id deyerine vermelisiniz. Eyni zamanda da,
+        Main terefde bu class'in instance'ini yaradib auto generate olunmus Id deyeri alina bilsin. 
+         */
+        private  int _id;
+        public int Id
+        {
+            get
+            {
+                GenerateId();
+                return _id;
+            }
+        }
+        private void GenerateId()
+        {
+            Random q = new Random();
+            _id = q.Next(1, 1000);
+        }
+
+    }
+}
