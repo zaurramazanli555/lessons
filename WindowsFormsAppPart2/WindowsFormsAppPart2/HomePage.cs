8b5e4ffd568d48175b246ebc4430ff579683a5ba
Orkhan Farajov <orkhankhf@gmail.com>
2022-02-04 21:51:40 +0300

    task

---
+++ WindowsFormsAppPart2/WindowsFormsAppPart2/HomePage.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace WindowsFormsAppPart2
+{
+    public partial class HomePage : Form
+    {
+        public HomePage(User user)
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ WindowsFormsAppPart2/WindowsFormsAppPart2/VirtualDatabase.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+
+namespace WindowsFormsAppPart2
+{
+    public class VirtualDatabase
+    {
+        public static List<User> Users = new List<User>();
+
+        static VirtualDatabase()
+        {
+            Users.Add(new User() {
+                Id = 1,
+                Name = "Orkhan",
+                Surname = "Farajov",
+                Username = "orkhanfarajov",
+                Password = "orkhan123456"
+            });
+
+            Users.Add(new User() {
+                Id = 2,
+                Name = "Ruslan",
+                Surname = "Valibayli",
+                Username = "ruslanvalibayli",
+                Password = "ruslan123456"
+            });
+
+            Users.Add(new User() {
+                Id = 3,
+                Name = "Zaur",
+                Surname = "Ramazanli",
+                Username = "zaurramazanli",
+                Password = "zaur123456"
+            });
+        }
+    }
+}
