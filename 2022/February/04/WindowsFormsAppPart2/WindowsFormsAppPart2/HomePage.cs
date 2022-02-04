82e78ea858b66e0f807ea8e1a2bd3d0941209daf
Orkhan Farajov <orkhankhf@gmail.com>
2022-02-05 02:18:47 +0300

    Win form part 2 and homework

---
+++ 2022/February/04/WindowsFormsAppPart2/WindowsFormsAppPart2/HomePage.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows.Forms;
+
+namespace WindowsFormsAppPart2
+{
+    public partial class HomePage : Form
+    {
+        public HomePage(User user)
+        {
+            InitializeComponent();
+            nameEdit.Text = user.Name;
+            surnameEdit.Text = user.Surname;
+            usernameEdit.Text = user.Username;
+            passwordEdit.Text = user.Password;
+            aboutUserEdit.Text = user.AboutUser;
+            saveUserInfoBtn.Tag = user.Id;
+        }
+
+        private void saveUserInfoBtn_Click(object sender, System.EventArgs e)
+        {
+            Button btn = (Button)sender;
+            User user = VirtualDatabase.Users.Find(m => m.Id == (int)btn.Tag);
+        }
+    }
+}
---
+++ 2022/February/04/WindowsFormsAppPart2/WindowsFormsAppPart2/LoginForm.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace WindowsFormsAppPart2
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LoginForm));
+            this.usernameLabel = new System.Windows.Forms.Label();
+            this.usernameTextBox = new System.Windows.Forms.TextBox();
+            this.passwordLabel = new System.Windows.Forms.Label();
+            this.passwordTextBox = new System.Windows.Forms.TextBox();
+            this.loginBtn = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // usernameLabel
+            // 
+            this.usernameLabel.AutoSize = true;
+            this.usernameLabel.Location = new System.Drawing.Point(168, 16);
+            this.usernameLabel.Name = "usernameLabel";
+            this.usernameLabel.Size = new System.Drawing.Size(66, 13);
+            this.usernameLabel.TabIndex = 0;
+            this.usernameLabel.Text = "İstifadəçi adı";
+            // 
+            // usernameTextBox
+            // 
+            this.usernameTextBox.Location = new System.Drawing.Point(240, 13);
+            this.usernameTextBox.Name = "usernameTextBox";
+            this.usernameTextBox.Size = new System.Drawing.Size(157, 20);
+            this.usernameTextBox.TabIndex = 1;
+            this.usernameTextBox.Enter += new System.EventHandler(this.TextboxEnterColor);
+            this.usernameTextBox.Leave += new System.EventHandler(this.TextboxLeaveColor);
+            // 
+            // passwordLabel
+            // 
+            this.passwordLabel.AutoSize = true;
+            this.passwordLabel.Location = new System.Drawing.Point(168, 55);
+            this.passwordLabel.Name = "passwordLabel";
+            this.passwordLabel.Size = new System.Drawing.Size(28, 13);
+            this.passwordLabel.TabIndex = 0;
+            this.passwordLabel.Text = "Şifrə";
+            // 
+            // passwordTextBox
+            // 
+            this.passwordTextBox.Location = new System.Drawing.Point(240, 52);
+            this.passwordTextBox.Name = "passwordTextBox";
+            this.passwordTextBox.PasswordChar = '*';
+            this.passwordTextBox.Size = new System.Drawing.Size(157, 20);
+            this.passwordTextBox.TabIndex = 2;
+            this.passwordTextBox.Enter += new System.EventHandler(this.TextboxEnterColor);
+            this.passwordTextBox.Leave += new System.EventHandler(this.TextboxLeaveColor);
+            // 
+            // loginBtn
+            // 
+            this.loginBtn.Location = new System.Drawing.Point(240, 92);
+            this.loginBtn.Name = "loginBtn";
+            this.loginBtn.Size = new System.Drawing.Size(157, 32);
+            this.loginBtn.TabIndex = 3;
+            this.loginBtn.Text = "Daxil ol";
+            this.loginBtn.UseVisualStyleBackColor = true;
+            this.loginBtn.Click += new System.EventHandler(this.Login);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(132, 148);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(421, 184);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.loginBtn);
+            this.Controls.Add(this.passwordTextBox);
+            this.Controls.Add(this.passwordLabel);
+            this.Controls.Add(this.usernameTextBox);
+            this.Controls.Add(this.usernameLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "LoginForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Giriş";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label usernameLabel;
+        private System.Windows.Forms.TextBox usernameTextBox;
+        private System.Windows.Forms.Label passwordLabel;
+        private System.Windows.Forms.TextBox passwordTextBox;
+        private System.Windows.Forms.Button loginBtn;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
